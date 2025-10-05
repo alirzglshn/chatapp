2949ef33738601dd8c4d17d7380a348fa177ab72
Alireza <golshandeveloper@gmail.com>
2025-10-05 17:20:03 +0330

    implemented the online count functionality

--- nameh/consumers.py
+++ nameh/consumers.py
@@ -1,55 +1,88 @@
+# consumers.py
 from channels.generic.websocket import WebsocketConsumer
 from django.shortcuts import get_object_or_404
-from .models import *
+from .models import ChatGroup, GroupMessage
 from asgiref.sync import async_to_sync
 from django.template.loader import render_to_string
 import json
 
-
 class ChatroomConsumer(WebsocketConsumer):
+
+    def update_online_count(self):
+        # show the real number of users in the M2M
+        online_count = self.chatroom.users_online.count() - 1
+
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
+
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+
+        context = {
+            'online_count': online_count,
+            'chat_group': self.chatroom,
+        }
+        html = render_to_string("nameh/partials/online_count.html", context)
+        # send HTML string; client JS will insert it into the DOM
+        self.send(text_data=html)
+
+
     def connect(self):
-        self.accept()
-        self.user = self.scope['user']
+        self.user = self.scope.get('user')
         self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name']
         self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
-        async_to_sync( self.channel_layer.group_add)(
-            self.chatroom_name , self.channel_name
-        )
+
+        async_to_sync(self.channel_layer.group_add)(self.chatroom_name, self.channel_name)
         self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.chatroom_name, self.channel_name
-        )
+        # only add if authenticated and not already in the M2M
+        if getattr(self.user, "is_authenticated", False) and self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
 
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(self.chatroom_name, self.channel_name)
+
+        # remove user when they disconnect (if authenticated)
+        if getattr(self.user, "is_authenticated", False) and self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
 
 
     def receive(self, text_data):
+        # only accept JSON messages like {"body": "..."}
+        try:
+            data = json.loads(text_data)
+        except json.JSONDecodeError:
+            return
 
-        data = json.loads(text_data)
         body = data.get('body')
+        if not body:
+            return
+
         message = GroupMessage.objects.create(
             body=body,
             author=self.user,
             group=self.chatroom
         )
-        context = {
-            'message'  : message ,
-            'user' : self.user
-        }
-        html = render_to_string("nameh/partials/chat_message_p.html", context)
-        self.send(text_data=html)
 
+        # broadcast to the group by message id so other consumers can render it with their context
         event = {
             'type': 'message_handler',
             'message_id': message.id,
         }
-        async_to_sync(self.channel_layer.group_send)(
-            self.chatroom_name, event
-        )
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
 
     def message_handler(self, event):
-        message_id = event['message_id']
+        message_id = event.get('message_id')
+        if not message_id:
+            return
         message = GroupMessage.objects.get(id=message_id)
         context = {
             'message': message,
--- nameh/views.py
+++ nameh/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 
 @login_required
 def chat_view(request, chatroom_name='visca-barca'):
@@ -9,13 +12,24 @@ def chat_view(request, chatroom_name='visca-barca'):
     chat_messages = chat_group.chat_messages.all()[:30]
     form = ChatmessageCreateForm()
 
-    if request.htmx:  # message submit
+    if request.htmx:
         form = ChatmessageCreateForm(request.POST)
         if form.is_valid():
             message = form.save(commit=False)
             message.author = request.user
             message.group = chat_group
             message.save()
+
+            # notify other websocket clients
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                chatroom_name,
+                {
+                    'type': 'message_handler',
+                    'message_id': message.id,
+                }
+            )
+
             return render(
                 request,
                 "nameh/partials/chat_message_p.html",
@@ -28,5 +42,7 @@ def chat_view(request, chatroom_name='visca-barca'):
         "chat_messages": chat_messages,
         "form": form,
         "chatroom_name": chatroom_name,
+        "online_count": chat_group.users_online.count(),
     }
+
     return render(request, "nameh/chat.html", context)
