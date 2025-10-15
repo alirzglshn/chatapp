598aadc9d89023e33b76efbfc81d8b7fa51e88c9
Alireza <golshandeveloper@gmail.com>
2025-10-15 22:46:39 +0330

    modified the chatgroup functionality

--- nameh/views.py
+++ nameh/views.py
@@ -27,8 +27,11 @@ def chat_view(request, chatroom_name='public-chat'):
 
     if chat_group.groupchat_name:
         if request.user not in chat_group.members.all():
-            chat_group.members.add(request.user)
-
+            if request.user.emailaddress_set.filter(verified=True).exists():
+                chat_group.members.add(request.user)
+            else:
+                messages.warning(request, 'You need to verify your email to join the chat!')
+                return redirect('profile-settings')
 
     if request.htmx:
         form = ChatmessageCreateForm(request.POST)
@@ -106,3 +109,58 @@ def create_groupchat(request):
         'form': form
     }
     return render(request, 'nameh/create_groupchat.html', context)
+
+
+@login_required
+def chatroom_edit_view(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if request.user != chat_group.admin:
+        raise Http404()
+
+    form = ChatRoomEditForm(instance=chat_group)
+
+    if request.method == 'POST':
+        form = ChatRoomEditForm(request.POST, instance=chat_group)
+        if form.is_valid():
+            form.save()
+
+            remove_members = request.POST.getlist('remove_members')
+            for member_id in remove_members:
+                member = User.objects.get(id=member_id)
+                chat_group.members.remove(member)
+
+            return redirect('chatroom', chatroom_name)
+
+    context = {
+        'form': form,
+        'chat_group': chat_group
+    }
+    return render(request, 'nameh/chatroom_edit.html', context)
+
+
+@login_required
+def chatroom_delete_view(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if request.user != chat_group.admin:
+        raise Http404()
+
+    if request.method == "POST":
+        chat_group.delete()
+        messages.success(request, 'Chatroom deleted')
+        return redirect('home')
+
+    return render(request, 'nameh/chatroom_delete.html', {'chat_group': chat_group})
+
+
+@login_required
+def chatroom_leave_view(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if request.user not in chat_group.members.all():
+        raise Http404()
+
+    if request.method == "POST":
+        chat_group.members.remove(request.user)
+        messages.success(request, 'You left the Chat')
+        return redirect('home')
+
+
