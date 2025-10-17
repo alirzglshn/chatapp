7d67028b02718f98a64574599b1e1eb114ae7215
Alireza <golshandeveloper@gmail.com>
2025-10-17 12:09:12 +0330

    implemented feature to send images

--- nameh/urls.py
+++ nameh/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import chat_view, get_or_create_chatroom, create_groupchat, chatroom_edit_view, chatroom_delete_view, \
-    chatroom_leave_view
+    chatroom_leave_view , chat_file_upload
 urlpatterns = [
     path("", chat_view, name="chat_view"),
     path('chat/new_groupchat/', create_groupchat, name="new-groupchat"),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('chat/edit/<chatroom_name>', chatroom_edit_view, name="edit-chatroom"),
     path('chat/delete/<chatroom_name>', chatroom_delete_view, name="chatroom-delete"),
     path('chat/leave/<chatroom_name>', chatroom_leave_view, name="chatroom-leave"),
+    path('chat/fileupload/<chatroom_name>', chat_file_upload, name="chat-file-upload"),
 ]
